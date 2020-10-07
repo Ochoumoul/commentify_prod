5ee8089110f897afd7a98ba5e5b9684897f70d55
Ochoumoul <pentestossa@gmail.com>
2020-10-08 00:59:54 +0100

    Updates

---
+++ precache-manifest.7828812e3828e787f91c1e71a4861ea5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ee64c438aaaaf7f537b034200d77ce",
+    "url": "/commentify_prod/index.html"
+  },
+  {
+    "revision": "63f8f9dc856f6fe917ed",
+    "url": "/commentify_prod/static/css/main.8a48e215.chunk.css"
+  },
+  {
+    "revision": "8bf7d14d3e88b8b6b3a1",
+    "url": "/commentify_prod/static/js/2.3a1cab68.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/commentify_prod/static/js/2.3a1cab68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f8f9dc856f6fe917ed",
+    "url": "/commentify_prod/static/js/main.4ed1db34.chunk.js"
+  },
+  {
+    "revision": "309244e1b5e5d83314a0",
+    "url": "/commentify_prod/static/js/runtime-main.3c6669a6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/commentify_prod/static/media/SEGA.12a3db8b.woff"
+  }
+]);
